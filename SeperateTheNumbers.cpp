60f7b2f8943d7a9e4d69bb06ea3e083d34bf8007
Evan <wanghaitang.ufl@gmail.com>
2017-07-01 14:57:14 -0400

    Checking Beautiful String

---
+++ SeperateTheNumbers.cpp
@@ -0,0 +1,8 @@
+/*
+Separate the numbers
+A numeric string, s, is beautiful if it can be split into a sequence of two or more 
+positive integers, a1, a2, ..., an, satisfying the following conditions:
+1. a_i - a_i-1 = 1 for any 1 <i <=numbers
+2. No a_i contains a leading zero.  like 1, 02, 03 are not beautiful
+3. The contents of the sequence cannot be rearranged.
+*/
